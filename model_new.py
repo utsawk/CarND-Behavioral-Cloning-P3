7c42ff90dfbfaeb50d213d98754b0992bcdefb75
utsawk <utsawk@gmail.com>
2018-06-27 21:31:46 -0700

    modify generator function to include image read on the fly

--- model_new.py
+++ model_new.py
@@ -4,6 +4,9 @@ import numpy as np
 import matplotlib.image as mpimg
 
 
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+
 # keras setup
 from keras.models import Model 
 from keras.layers import Flatten, Dense, Lambda, Cropping2D, Activation, Dropout, Input, concatenate
@@ -21,15 +24,20 @@ def brightness_augment_image(image):
     image1[:,:,2][image1[:,:,2]>255]  = 255
     image1 = np.array(image1, dtype = np.uint8)
     image1 = cv2.cvtColor(image1,cv2.COLOR_HSV2RGB)
-    return image1
+    return image
 
-def perturb_image(image, steer, perturb_range_hor, perturb_range_ver):
-    dx = perturb_range_hor*np.random.uniform()-perturb_range_hor/2
-    steer_ang = steer + dx * 0.004
-    dy = perturb_range_ver*np.random.uniform()-perturb_range_ver/2
+def translate_image(image, angle, translate_range_hor, translate_range_ver):
+    dx = translate_range_hor*np.random.uniform()-translate_range_hor/2
+    dy = translate_range_ver*np.random.uniform()-translate_range_ver/2
     transform_matrix = np.float32([[1,0,dx],[0,1,dy]])
     image = cv2.warpAffine(image,transform_matrix,(image.shape[1], image.shape[0]))
-    return image,steer_ang
+    angle = angle + dx * 0.004
+    return image,angle
+
+def perturb_image_helper(image, angle):
+    image = brightness_augment_image(image)
+    image, angle = translate_image(image, angle, translate_range_hor, translate_range_ver)
+    return image,angle
 
 
 lines = []
@@ -38,52 +46,46 @@ with open('data/driving_log.csv') as csvfile:
     for line in reader:
         lines.append(line)
 
-images = []
-measurements = []
 correction = [0, 0.25, -0.25]
-perturb_range_hor = 100
-perturb_range_ver = 20
+translate_range_hor = 100
+translate_range_ver = 20
 
-for line in lines[1:]:
-    for i in range(3):
-        source_path = line[i]
-        filename = source_path.split('/')[-1]
-        current_path = 'data/IMG/' + filename
-        image = mpimg.imread(current_path)
-        images.append(image)
-        measurement = float(line[3]) + correction[i]
-        measurements.append(measurement)
-
-X = np.array(images)
-y = np.array(measurements)
-
-from sklearn.utils import shuffle
-from sklearn.model_selection import train_test_split
-X_train, X_validation, y_train, y_validation = train_test_split(X, y, test_size=0.2)
+train_samples, validation_samples = train_test_split(lines, test_size=0.2)
 
 # add flipped image and the measurement for data augmentation with 1/2 probability
-def generator_train(X, y, batch_size):
-    num_samples = len(X)
+def generator(samples, batch_size, training):
+    num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
-        X, y = shuffle(X,y)
+        samples = shuffle(samples)
         for offset in range(0, num_samples, batch_size):
-            X_samples = X[offset:offset+batch_size]
-            y_samples = y[offset:offset+batch_size]
+            batch_samples = samples[offset:offset+batch_size]
             images = []
             measurements = []
-            for i in range(len(X_samples)):
-                image = brightness_augment_image(X_samples[i])
-                image, measurement = perturb_image(image, y_samples[i], perturb_range_hor, perturb_range_ver)
-                if(np.random.uniform() > 0.5):
+            for sample in batch_samples:
+                for i in range(3):
+                    source_path = sample[i]
+                    filename = source_path.split('/')[-1]
+                    current_path = 'data/IMG/' + filename
+                    image = mpimg.imread(current_path)
+                    measurement = float(line[3]) + correction[i]
+                    # copy for processing later
+                    image_copy = np.copy(image)
+                    measurement_copy = measurement
+                    if training:
+                        image, measurement = perturb_image_helper(image, measurement)
                     images.append(image)
                     measurements.append(measurement)
-                else:
-                    images.append(np.fliplr(image))
-                    measurements.append(-measurement)
-                    
-            X_train = np.array(images)
-            y_train = np.array(measurements)
-            yield shuffle(X_train, y_train)
+                    # add flipped image and the measurement for data augmentation
+                    image = np.fliplr(image_copy)
+                    measurement = -measurement_copy
+                    if training:
+                        image, measurement = perturb_image_helper(image, measurement)
+                    images.append(image)
+                    measurements.append(measurement)
+                   
+            X = np.array(images)
+            y = np.array(measurements)
+            yield shuffle(X, y)
 
 # dropout probabilities
 dropout_prob_cl = 0.25
@@ -92,7 +94,8 @@ dropout_prob_fc = 0.5
 batch_size = 128
 
 # compile and train the model using the generator function
-train_generator = generator_train(X_train, y_train, batch_size)
+train_generator = generator(train_samples, batch_size, True)
+validation_generator = generator(validation_samples, batch_size, False)
 
 inputs = Input(shape = (160, 320, 3))
 preprocess = Lambda(lambda x: x / 255.0 - 0.5)(inputs)
@@ -127,6 +130,7 @@ prediction = Dense(1)(fc1)
 model = Model(inputs = inputs, outputs = prediction)
 
 model.compile(loss = 'mse', optimizer = 'adam')
-model.fit_generator(train_generator, steps_per_epoch= len(X_train)//batch_size, validation_data=(X_validation, y_validation),  validation_steps=len(X_validation)//batch_size, nb_epoch=10)
+model.fit_generator(train_generator, steps_per_epoch= len(train_samples)//batch_size, \
+    validation_data=validation_generator, validation_steps=len(validation_samples)//batch_size, nb_epoch=10)
 model.save('model_track2.h5')
 model.summary()
